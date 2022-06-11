21f9b7225904d815226d44fe9a76197a021b1d55
Kartik Shukla <kaartikshukla7120@gmail.com>
2022-06-11 15:04:21 +0530

    Added bcrypt js and addeda fake database

---
+++ fakeDB.js
@@ -0,0 +1,12 @@
+module.exports = [
+    {
+        username: "Kaartik",
+        favAnime: "PSYCHO-PASS",
+        password: "c068c3f56a994993e5f98b7ce294c0be"
+    },
+    {
+        username: "Aman",
+        favAnime: "Jujitsu Kaisen",
+        password: "07c6b85f22e9858e336963ebc3f62b83"
+    }
+]
