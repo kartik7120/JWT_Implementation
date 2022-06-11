ea6ad756a7efd97b61fc1aa495b29927ded9e9fd
Kartik Shukla <kaartikshukla7120@gmail.com>
2022-06-11 16:38:25 +0530

    Added a login route and returned a token

--- app.js
+++ app.js
@@ -55,6 +55,39 @@ app.post("/register", [
     res.json(token);
 })
 
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+
+    let user = fakeDB.find(user => user.email === email);
+
+    if (!user) {
+        return res.status(400).json({
+            errors: {
+                msg: "email or password is not correct"
+            }
+        })
+    }
+
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if (!isValid) {
+        return res.status(400).json({
+            errors: {
+                msg: "email or password is not correct"
+            }
+        })
+    }
+
+    const token = await jwt.sign({
+        email,
+        msg: "User is logged in"
+    }, process.env.SECRET, {
+        expiresIn: "30"
+    })
+
+    res.json(token);
+})
+
 app.listen(process.env.PORT, () => {
     console.log(`Listening on port ${process.env.PORT}`);
 })
