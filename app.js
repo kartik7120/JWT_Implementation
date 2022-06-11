5144474096b763d51dc8ae82f97660758b465e82
Kartik Shukla <kaartikshukla7120@gmail.com>
2022-06-11 15:23:38 +0530

    Added a register route in the app

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 require("dotenv").config();
 const express = require("express");
+const fakeDB = require("./fakeDB");
 const jwt = require("jsonwebtoken");
 const app = express();
 
+app.use(express.json());
+
 app.get("/hello", (req, res) => {
     res.send("HELLO");
+})
+
+app.post("/register", async (req, res) => {
+    const { username, password } = req.body;
+    console.log(username, password);
+    res.json("Register route");
 })
 
 app.listen(process.env.PORT, () => {
