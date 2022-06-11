29c3ddd38a62592b599ff5f522a9636f739e0ac1
Kartik Shukla <kaartikshukla7120@gmail.com>
2022-06-11 13:55:50 +0530

    Added a get request hello route

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 const app = express();
 
+app.get("/hello", (req, res) => {
+    res.send("HELLO");
+})
+
 app.listen(process.env.PORT, () => {
     console.log(`Listening on port ${process.env.PORT}`);
 })
