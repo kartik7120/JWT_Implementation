3e55b624c86a9ed77df52ae2b4bdc39a7229dfd5
Kartik Shukla <kaartikshukla7120@gmail.com>
2022-06-11 16:52:15 +0530

    Added some public and private posts in fakeDB file

--- fakeDB.js
+++ fakeDB.js
@@ -1,4 +1,4 @@
-module.exports = [
+const userData = [
     {
         email: "Kaartik@gmail.com",
         password: "c068c3f56a994993e5f98b7ce294c0be"
@@ -8,3 +8,37 @@ module.exports = [
         password: "07c6b85f22e9858e336963ebc3f62b83"
     }
 ]
+const publicPosts = [
+    {
+        title: "Free tips on development",
+        content: "These are some tips to become a better developer"
+    },
+    {
+        title: "Free tips on development",
+        content: "These are some tips to become a better developer"
+    },
+    {
+        title: "Free tips on development",
+        content: "These are some tips to become a better developer"
+    }
+]
+
+const privatePosts = [
+    {
+        title: "Paid tips on development",
+        content: "These are some paid tips to become a better developer"
+    },
+    {
+        title: "Paid tips on development",
+        content: "These are some paid tips to become a better developer"
+    },
+    {
+        title: "Paid tips on development",
+        content: "These are some paid tips to become a better developer"
+    }
+]
+module.exports = {
+    userData,
+    publicPosts,
+    privatePosts
+}
